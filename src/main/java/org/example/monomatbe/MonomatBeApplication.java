3409bde922d75dabae549bfd511ae4aed2401a22
pjh213 <mine1594@naver.com>
2026-04-07 19:03:03 +0900

    feat(#1): 초기 프로젝트 세팅

---
+++ src/main/java/org/example/monomatbe/MonomatBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.monomatbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonomatBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonomatBeApplication.class, args);
+    }
+
+}

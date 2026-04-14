d3eef23b2dc76d98b78660a9149d44ae677a70a4
pjh213 <mine1594@naver.com>
2026-04-14 14:36:02 +0900

    feat(#12): 버그 수정

--- src/main/java/io/github/ascrew/monomatbe/service/RedisPublisher.java
+++ src/main/java/io/github/ascrew/monomatbe/service/RedisPublisher.java
@@ -1,5 +1,6 @@
 package io.github.ascrew.monomatbe.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.ascrew.monomatbe.dto.ChatMessageDto;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -9,13 +10,23 @@ import org.springframework.stereotype.Service;
 public class RedisPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
 
     public RedisPublisher(RedisTemplate<String , Object> redisTemplate) {
+        this.objectMapper = new ObjectMapper();
         this.redisTemplate = redisTemplate;
     }
 
     public void publish(String topic, ChatMessageDto messageDto) {
-        // Redis의 특정 채널(topic)에 메시지를 발행
-        redisTemplate.convertAndSend(topic, messageDto);
+        try {
+            String JsonMessage = objectMapper.writeValueAsString(messageDto);  //ChatMessageDto 객체를 JSON 문자열로 직렬화
+
+            // Redis의 특정 채널(topic)에 메시지를 발행
+            redisTemplate.convertAndSend(topic, JsonMessage);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
